61708594f84f6e98e0d78505ec909528d0a20f1a
Yamashita, Yuu <peek824545201@gmail.com>
2016-02-16 04:35:06 +0000

    Allow users to configure file's owner/group/mode

--- test/fixtures/app/recipes/default.rb
+++ test/fixtures/app/recipes/default.rb
@@ -2,4 +2,8 @@ artifactory_artifact "/opt/twittersdk/twitter-core-1.6.4-javadoc.jar" do
   artifactoryonline "twittersdk"
   repository "repo"
   repository_path "com/twitter/sdk/android/twitter-core/1.6.4/twitter-core-1.6.4-javadoc.jar"
+
+  owner "root"
+  group "root"
+  mode "0644"
 end
