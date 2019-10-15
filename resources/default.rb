ad7eb61b2694602546db9702ec963061281b2f78
Vijayandra Patil <patvij01@CSGICORP.COM>
2019-10-15 13:58:17 +0530

    Added feature to cookbook , now you can use resource to download highest versioned artifact from artifactory

--- resources/default.rb
+++ resources/default.rb
@@ -3,25 +3,78 @@ default_action :create
 property :artifactory_url, :kind_of => String
 property :artifactoryonline, :kind_of => String
 property :repository, :kind_of => String, :required => true
-property :repository_path, :kind_of => String, :required => true
+property :artifact_name, :kind_of => String, :required => true
 property :artifactory_username, :kind_of => String
 property :artifactory_password, :kind_of => String
-
+property :highest, :kind_of => [TrueClass, FalseClass]
 property :group, :kind_of => [Integer, String]
 property :mode, :kind_of => [Integer, String]
 property :owner, :kind_of => [Integer, String]
 
 action_class do
   require "uri"
+  require "net/http"
+  require "pp"
+  require "json"
 
   include ::ArtifactoryArtifact::Helper
 
+
+  def fetch_highest (new_resource)
+    #Fetching list of artifacts from artifactory 
+    url = URI("#{new_resource.artifactory_url}/api/search/aql/")
+    http = Net::HTTP.new(url.host, url.port)
+
+     request = Net::HTTP::Post.new(url)
+     request["Content-Type"] = 'text/plain'
+     request["Accept"] = '*/*'
+     request["Host"] = "#{node['chef-artifactory-artifact']['host']}"
+     request["Connection"] = 'keep-alive'
+     request.basic_auth new_resource.artifactory_username, new_resource.artifactory_password 
+     request.body = "items.find({\"repo\":{\"$eq\":\"   #{new_resource.repository}\"}})"
+
+     response = http.request(request)
+     parsed_json = JSON.parse(response.body)
+
+     name_re = new_resource.artifact_name.gsub(/HIGHEST/, '([\.0-9]+)')
+     puts "name_re: #{name_re}"
+
+     #Adding Version tag to parsed_json
+    parsed_json["results"].each do |res|
+     output = res["name"]
+     output =~ /^#{name_re}$/
+     res['version'] = $1
+    end
+ 
+    #Sorting versions and picking highest value
+    repos = parsed_json['results']
+    versions = repos.map { |x| x.values[-1] }
+    puts "---------------------------------------"
+    versions = versions.reject { |item| item.nil? || item == '' }
+    pp versions
+    highest_version = versions.sort! { |x,y|
+      Chef::Provider::Package::Yum::RPMUtils.rpmvercmp(x['version'], y['version'])
+    } 
+    highest_version = highest_version.last
+    puts "---------------------------------------"
+    pp highest_version
+    highest_versioned_artifact = repos.find {|h1| h1['version']==highest_version}['name']
+    puts "-------------------------------------------------------------------------------------------------------------"
+    puts "Highest versioned artifact is #{highest_versioned_artifact}"
+    puts "-------------------------------------------------------------------------------------------------------------"
+    new_resource.artifact_name = "/#{highest_versioned_artifact}"
+  end 
+
   def manage_resource(new_resource)
+    if new_resource.highest 
+     fetch_highest(new_resource)
+    end
+    
     request_headers = artifactory_headers(
       :username => new_resource.artifactory_username,
       :password => new_resource.artifactory_password,
     )
-
+    
     if new_resource.artifactory_url
       artifactory_url = ::URI.parse(new_resource.artifactory_url)
     else
@@ -32,9 +85,9 @@ action_class do
       end
     end
 
-    repository_path = "#{new_resource.repository.sub(/\A\/+/, "")}/#{new_resource.repository_path}"
-    artifact_url = ::URI.join("#{artifactory_url}/", repository_path.sub(/\A\/+/, ""))
-    storage_url = ::URI.join("#{artifactory_url}/", "api/storage/#{repository_path}")
+    artifact_name = "#{new_resource.repository.sub(/\A\/+/, "")}/#{new_resource.artifact_name}"
+    artifact_url = ::URI.join("#{artifactory_url}/", artifact_name.sub(/\A\/+/, ""))
+    storage_url = ::URI.join("#{artifactory_url}/", "api/storage/#{artifact_name}")
 
     # Retrieve Artifact's SHA256 checksum via Artifactory REST API
     # https://www.jfrog.com/confluence/display/RTF/Artifactory+REST+API#ArtifactoryRESTAPI-FileInfo
--- test/fixtures/app/recipes/default.rb
+++ test/fixtures/app/recipes/default.rb
@@ -6,3 +6,14 @@ artifactory_artifact "/opt/commons-httpclient/commons-httpclient-3.1.jar" do
   group "root"
   mode "0644"
 end
+
+
+#testing to fetch highest versioned artifact from artifactory
+artifactory_artifact "/path/reponame.zip" do
+  artifactory_url ""
+  repository ""
+  artifact_name "XYZ-HIGHEST.zip"
+  highest true
+  artifactory_username ""
+  artifactory_password ""
+end
