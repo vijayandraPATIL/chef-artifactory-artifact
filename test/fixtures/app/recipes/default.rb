524fede12663dd2369aa36bab1180ea400d5a55a
Vijayandra Patil <patvij01@CSGICORP.COM>
2019-10-21 21:34:14 +0530

    Modified test cases

--- test/fixtures/app/recipes/default.rb
+++ test/fixtures/app/recipes/default.rb
@@ -10,11 +10,25 @@ end
 
 # Testing to fetch highest versioned artifact from Artifactory
 # I have tested this from my end and it works fine , any tester can do as long as they have an internal Artifactory.
-artifactory_artifact "/opt/common-httpclient.zip" do
-  artifactoryonline "grails"
-  repository "core"
-  artifact_name "commons-httpclient-HIGHEST.zip"
+artifactory_artifact "/opt/test_file_highest.zip" do
+  artifactory_url node['artifactory_url']
+  repository "chnl-DolceLexarApp1-Dev-generic"
+  artifact_name "DolceLexarPolicy-Policies-HIGHEST.zip"
   highest true
+  artifactory_username node['artifactory_username']
+  artifactory_password node['artifactory_password']
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+#testing to fetch highest versioned artifact from artifactory
+artifactory_artifact "/opt/testfile.zip" do
+  artifactory_url node['artifactory_url']
+  repository "chnl-DolceLexarApp1-Dev-generic"
+  repository_path "/DolceLexarPolicy-Policies-19.3.0.34.zip"
+  artifactory_username node['artifactory_username']
+  artifactory_password node['artifactory_password']
   owner "root"
   group "root"
   mode "0644"
--- test/integration/default/bash/spec/localhost/server_spec.rb
+++ test/integration/default/bash/spec/localhost/server_spec.rb
@@ -5,3 +5,7 @@ set :backend, :exec
 describe file("/opt/commons-httpclient/commons-httpclient-3.1.jar") do
   it { should be_file }
 end
+
+describe file("/opt/testfile.zip") do
+  it { should be_file }
+end
